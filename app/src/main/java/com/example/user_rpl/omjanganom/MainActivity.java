dbf7e9ce9fcb63185f52412a57568021467b3048
wahyurmd2100 <wahyuarema7@gmail.com>
2019-09-25 10:41:03 +0700

    edit kedua

--- app/src/main/java/com/example/user_rpl/omjanganom/MainActivity.java
+++ app/src/main/java/com/example/user_rpl/omjanganom/MainActivity.java
@@ -2,8 +2,17 @@ package com.example.user_rpl.omjanganom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editName, editPassword;
+    TextView result;
+    Button buttonSubmit, buttonReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
